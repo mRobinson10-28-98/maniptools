16f24ac726cb1baebc9106b412e83a3fef088df4
Matthew Robinson <matt.robinson10.28.98@gmail.com>
2025-08-28 12:19:05 -0600

    Dual numbers tools working for numbers and vectors

--- include/DualNumberTools.hpp
+++ include/DualNumberTools.hpp
@@ -7,35 +7,15 @@ namespace DualNumberTools
 {
     struct DualNumber
     {
-        DualNumber()
-        {
-            real = 0;
-            dual = 0;
-        }
+        DualNumber();
 
-        DualNumber(double r, double d) 
-        {
-            real = r;
-            dual = d;
-        }
+        DualNumber(double r, double d);
 
-        DualNumber operator+(const DualNumber other) const 
-        {
-            DualNumber d_sum(real + other.real, dual + other.dual);
-            return d_sum;
-        }
+        DualNumber operator+(const DualNumber other) const;
 
-        DualNumber operator-(const DualNumber other) const 
-        {
-            DualNumber d_min(real - other.real, dual - other.dual);
-            return d_min;
-        }
+        DualNumber operator-(const DualNumber other) const;
 
-        DualNumber operator*(const DualNumber other) const 
-        {
-            DualNumber d_prod(real * other.real, real * other.dual + dual * other.real);
-            return d_prod;
-        }
+        DualNumber operator*(const DualNumber other) const;
 
         double real;
         double dual;
@@ -43,76 +23,33 @@ namespace DualNumberTools
 
     struct DualVector
     {
-        DualVector()
-        {
-            real = Eigen::Vector3d::Zero();
-            dual = Eigen::Vector3d::Zero();
-        }
+        DualVector();
 
-        DualVector(Eigen::Vector3d r, Eigen::Vector3d d) 
-        {
-            real = r;
-            dual = d;
-        }
+        DualVector(Eigen::Vector3d r, Eigen::Vector3d d);
 
-        DualNumber operator[](std::size_t index) const
-        {
-            if (index >= 3) throw std::out_of_range("Dual Vector index out of range");
-            DualNumber d(real[index], dual[index]);
+        DualNumber operator[](std::size_t index) const;
 
-            return d;
-        }
+        DualVector operator+(const DualVector other) const;
 
-        DualVector operator+(const DualVector other) const 
-        {
-            DualVector d_sum(real + other.real, dual + other.dual);
-            return d_sum;
-        }
-
-        DualVector operator-(const DualVector other) const 
-        {
-            DualVector d_min(real - other.real, dual - other.dual);
-            return d_min;
-        }
+        DualVector operator-(const DualVector other) const;
 
         // Dual vector dot product = dual number
-        DualNumber operator*(const DualVector other) const 
-        {
-            DualNumber d_prod;
-            d_prod = ((*this)[0] * other[0]) + ((*this)[1] * other[1]) + ((*this)[2] * other[2]);
-            return d_prod;
-        }
+        DualNumber operator*(const DualVector other) const;
 
         // Dual number * dual vector product = dual vector
-        DualVector operator*(const DualNumber other) const 
-        {
-            DualNumber d_prod;
-            d_prod = ((*this)[0] * other[0]) + ((*this)[1] * other[1]) + ((*this)[2] * other[2]);
-            return d_prod;
-        }
+        DualVector operator*(const DualNumber other) const;
 
+        DualVector cross(DualVector d) const;
 
         Eigen::Vector3d real;
         Eigen::Vector3d dual;
     };
 
-    DualVector DualVectorCrossProduct(DualVector d1, DualVector d2);
-
-    // DualVector DualNumberDualVectorProduct(DualNumber d1, DualVector d2);
-
     struct DualQuaternion
     {
-        DualQuaternion()
-        {
-            real = Eigen::Quaterniond::Identity();
-            dual = Eigen::Quaterniond::Identity();
-        }
+        DualQuaternion();
 
-        DualQuaternion(Eigen::Quaterniond r, Eigen::Quaterniond d) 
-        {
-            real = r;
-            dual = d;
-        }
+        DualQuaternion(Eigen::Quaterniond r, Eigen::Quaterniond d);
 
         Eigen::Quaterniond real;
         Eigen::Quaterniond dual;
--- src/DualNumberTools.cpp
+++ src/DualNumberTools.cpp
@@ -2,17 +2,117 @@
 
 using namespace DualNumberTools;
 
-DualVector DualVectorCrossProduct(DualVector d1, DualVector d2)
+// - - -
+// Dual Numbers
+// - - -
+
+DualNumber::DualNumber()
+{
+    real = 0;
+    dual = 0;
+}
+
+DualNumber::DualNumber(double r, double d) 
+{
+    real = r;
+    dual = d;
+}
+
+DualNumber DualNumber::operator+(const DualNumber other) const 
+{
+    DualNumber d_sum(real + other.real, dual + other.dual);
+    return d_sum;
+}
+
+DualNumber DualNumber::operator-(const DualNumber other) const 
+{
+    DualNumber d_min(real - other.real, dual - other.dual);
+    return d_min;
+}
+
+DualNumber DualNumber::operator*(const DualNumber other) const 
+{
+    DualNumber d_prod(real * other.real, real * other.dual + dual * other.real);
+    return d_prod;
+}
+
+// - - -
+// Dual Vectors
+// - - -
+
+DualVector::DualVector()
+{
+    real = Eigen::Vector3d::Zero();
+    dual = Eigen::Vector3d::Zero();
+}
+
+DualVector::DualVector(Eigen::Vector3d r, Eigen::Vector3d d) 
+{
+    real = r;
+    dual = d;
+}
+
+DualNumber DualVector::operator[](std::size_t index) const
+{
+    if (index >= 3) throw std::out_of_range("Dual Vector index out of range");
+    DualNumber d(real[index], dual[index]);
+
+    return d;
+}
+
+DualVector DualVector::operator+(const DualVector other) const 
+{
+    DualVector d_sum(real + other.real, dual + other.dual);
+    return d_sum;
+}
+
+DualVector DualVector::operator-(const DualVector other) const 
+{
+    DualVector d_min(real - other.real, dual - other.dual);
+    return d_min;
+}
+
+// Dual vector dot product = dual number
+DualNumber DualVector::operator*(const DualVector other) const 
+{
+    DualNumber d_prod;
+    d_prod = ((*this)[0] * other[0]) + ((*this)[1] * other[1]) + ((*this)[2] * other[2]);
+    return d_prod;
+}
+
+// Dual number * dual vector product = dual vector
+DualVector DualVector::operator*(const DualNumber other) const 
+{
+    DualVector d_prod;
+    d_prod[0] = other * (*this)[0];
+    d_prod[1] = other * (*this)[1];
+    d_prod[2] = other * (*this)[2];
+
+    return d_prod;
+}
+
+DualVector DualVector::cross(DualVector d2) const
 {
     DualVector d_cross;
-    d_cross[0] = d1[1] * d2[2] - d1[2] * d2[1];
-    d_cross[1] = d1[0] * d2[2] - d1[2] * d2[0];
-    d_cross[2] = d1[0] * d2[1] - d1[1] * d2[0];
-    
+    d_cross[0] = (*this)[1] * d2[2] - (*this)[2] * d2[1];
+    d_cross[1] = (*this)[0] * d2[2] - (*this)[2] * d2[0];
+    d_cross[2] = (*this)[0] * d2[1] - (*this)[1] * d2[0];
+
     return d_cross;
 }
 
-DualVector DualNumberDualVectorProduct(DualNumber d1, DualVector d2)
-{
+// - - -
+// Dual Quaternions
+// - - -
 
+DualQuaternion::DualQuaternion()
+{
+    real = Eigen::Quaterniond::Identity();
+    dual = Eigen::Quaterniond::Identity();
+}
+
+DualQuaternion::DualQuaternion(Eigen::Quaterniond r, Eigen::Quaterniond d) 
+{
+    real = r;
+    dual = d;
 }
